9dac3a525441a91e779cbc4e735b079c9276dfb9
Werner Gehrke Junior <werner.junior@outlook.com>
2019-08-08 23:26:49 -0300

    first commit

---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+import './Login.css';
+
+export default function Login() {
+    return (
+        <img src={logo} alt="Tindev" />
+    );
+}
